c0df79f4ff4c2aff1b7b5971c3d3642b5f76d381
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-06 22:01:49 +0530

    Create Numbersignidentifier.java

---
+++ Numbersignidentifier.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Numbersignidentifier {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n > 0){
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
