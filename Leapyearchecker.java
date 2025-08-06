7dbfb010e568e3ff67bf8931a00bae9c2eb426c3
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-06 22:00:21 +0530

    Create Leapyearchecker.java

---
+++ Leapyearchecker.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Leapyearchecker {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not a Leap Year");
+    }
+}
+     
