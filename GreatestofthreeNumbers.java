4edb0bba55e2f6f1ceddaa92cc3e904affb77363
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-06 21:53:55 +0530

    Create GreatestofthreeNumbers.java

---
+++ GreatestofthreeNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GreatestofthreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a == b && b == c){
+            System.out.println("There is a tie for the greatest number");
+        }
+        else if (a == b && a > c){
+            System.out.println("There is a tie for the greatest number");
+        } else if (a == c && a > b){
+            System.out.println("There is a tie for the greatest number");
+        } else if (b == c && b > a){
+            System.out.println("There is a tie for the greatest number");
+        }
+        else if (a > b && a > c){
+            System.out.println(a + " is the greatest");
+        } else if (b > a && b > c){
+            System.out.println(b + " is the greatest");
+        } else {
+            System.out.println(c + " is the greatest");
+        }
+        scanner.close();
+    }
+}
