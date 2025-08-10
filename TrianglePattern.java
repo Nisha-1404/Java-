2b33df38c04c261d08aba047ed9210bd69d25c07
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-10 19:47:27 +0530

    Create TrianglePattern.java

---
+++ TrianglePattern.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Print i stars in the i-th row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to next line
+        }
+    }
+}
