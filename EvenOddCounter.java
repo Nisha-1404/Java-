54622124dfb69f91f47a2e8bfda78e4f433f0202
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-10 20:20:40 +0530

    Create EvenOddCounter.java

---
+++ EvenOddCounter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println(evenCount);
+        System.out.println(oddCount);
+        
+        sc.close();
+    }
+}
