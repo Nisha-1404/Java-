b39f3468df40c7937412f738ff1791f24f90f18f
NISHA.S <nishas22ec@psnacet.edu.in>
2025-08-21 22:25:05 +0530

    Create SecondLargestDistinct.java

---
+++ SecondLargestDistinct.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SecondLargestDistinct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+        
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(secondLargest);
+        }
+        
+        sc.close();
+    }
+}
